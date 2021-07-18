2ca22d62d9fa2c299f68e21ae67829ffe1dd48a2
Your Name <you@example.com>
2021-07-19 01:21:09 +0700

    make it better

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const UserRoute = require('./UserRoute')
 const PostRoute = require('./PostRoute')
 
 const route =(app)=>{
+    app.use('/home',(req,res)=>res.send('hello'))
     app.use('/api/user',UserRoute)
     app.use('/api/post',PostRoute)
 }
