f55f4c911b58c9089c0941422bf26172a4931baa
Your Name <you@example.com>
2021-07-19 12:38:23 +0700

    19/7/2021

--- src/routes/PostRoute.js
+++ src/routes/PostRoute.js
@@ -1,12 +1,15 @@
+  
 const express = require('express')
 const PostCtrl=require('../app/controller/PostCtrl')
 const auth=require('../app/middleware/auth')
 const router = express.Router();
 
 router.post('/',auth,PostCtrl.createPost)
+router.post('/filecomment',auth,PostCtrl.filecomment)
 router.get('/getpost',PostCtrl.getPosts)
 router.post('/delete',auth,PostCtrl.deletePost)
 router.get('/getpostbyuserid/:id',PostCtrl.getpostbyuserid)
+router.get('/getpostbyid/:id',PostCtrl.getpostbyid)
 
 
 module.exports=router
